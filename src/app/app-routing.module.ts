5233aa7d039cfd3ebe35eef01292fad4bac23848
sabaath <babic.5832@gmail.com>
2023-05-16 22:06:33 +0200

    redesigned login and register

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ConversationComponent } from './conversation/conversation.component';
-import { LoginComponent } from './login/login.component';
-import { RegisterComponent } from './register/register.component';
 import {HubComponent} from "./hub/hub.component";
 import {MainComponent} from "./main/main.component";
 import {InviteComponent} from "./invite/invite.component";
+import {LoginPageComponent} from "./login/login-page/login-page.component";
+import {RegisterPageComponent} from "./register/register-page/register-page.component";
 
 const routes: Routes = [
   { path: '', redirectTo: '/main', pathMatch: 'full' },
-  { path: 'login', component: LoginComponent },
-  { path: 'register', component: RegisterComponent },
+  { path: 'login', component: LoginPageComponent },
+  { path: 'register', component: RegisterPageComponent },
   { path: 'conversation/:id', component: ConversationComponent },
   { path: 'hub', component: HubComponent},
   { path: 'main', component: MainComponent},
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
-import { FormsModule } from "@angular/forms";
-import { RegisterComponent } from './register/register.component';
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import { ConversationService } from './common/service/conversation.service';
 import { ConversationComponent } from './conversation/conversation.component';
 import { HttpClientModule } from "@angular/common/http";
@@ -13,26 +10,44 @@ import { HubComponent } from './hub/hub.component';
 import { RouterModule } from '@angular/router';
 import { MainComponent } from './main/main.component';
 import { InviteComponent } from './invite/invite.component';
+import { LoginFormComponent } from './login/login-form/login-form.component';
+import { LoginPageComponent } from './login/login-page/login-page.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatButtonModule} from "@angular/material/button";
+import {MatInputModule} from "@angular/material/input";
+import {MatCardModule} from "@angular/material/card";
+import { RegisterFormComponent } from './register/register-form/register-form.component';
+import { RegisterPageComponent } from './register/register-page/register-page.component';
+import {_MatCheckboxRequiredValidatorModule, MatCheckboxModule} from "@angular/material/checkbox";
 
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent,
-    RegisterComponent,
     ConversationComponent,
     HubComponent,
     MainComponent,
     InviteComponent,
+    LoginFormComponent,
+    LoginPageComponent,
+    RegisterFormComponent,
+    RegisterPageComponent,
   ],
-    imports: [
-      BrowserModule,
-      FormsModule,
-      HttpClientModule,
-      AppRoutingModule,
-      RouterModule
-    ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    AppRoutingModule,
+    RouterModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatInputModule,
+    MatCardModule,
+    _MatCheckboxRequiredValidatorModule,
+    MatCheckboxModule
+  ],
   providers: [
     ConversationService
   ],
